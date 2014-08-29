bbab50f8726e1c299b9051252fb2429a8b9f41b0
Arild Matsson <arild@klavaro.se>
2014-08-29 17:53:02 +0200

    Defining backend factory in Settings

--- src/Tests/PastKernelTest.php
+++ src/Tests/PastKernelTest.php
@@ -7,6 +7,7 @@
 
 namespace Drupal\past\Tests;
 
+use Drupal\Core\Site\Settings;
 use Drupal\Core\Utility\Error;
 use Drupal\simpletest\KernelTestBase;
 use Drupal\past\PastEventInterface;
@@ -50,6 +51,9 @@ class PastKernelTest extends KernelTestBase {
     $this->installSchema('past_db', array('past_event_argument', 'past_event_data'));
     $this->installSchema('system', 'sequences');
     $this->config = \Drupal::config('past.settings');
+    new Settings(Settings::getAll() + array(
+      'past_backend' => 'past_db_backend',
+    ));
   }
 
   /**
